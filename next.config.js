3c1d486c308e996a5c93ee99dd9eb95481898834
kugankuganesh <kugankuganesh@gmail.com>
2026-02-03 10:59:52 +0530

    remove

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
-module.exports = {
-  basePath,
-  assetPrefix: basePath,
-  images: {
-    unoptimized: true, // needed for next export / static hosting
-  },
-};
